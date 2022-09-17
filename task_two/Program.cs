dddb212f475a65cf52425552286c885faf5f164b
Andrew <it.andr@outlook.com>
2022-09-17 21:27:45 +0300

    second task

---
+++ task_two/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+string input = Console.ReadLine();
+int num1 = int.Parse(input);
+
+Console.Write("Введите второе число: ");
+string input2 = Console.ReadLine();
+int num2 = int.Parse(input2);
+
+Console.Write("Введите третье число: ");
+string input3 = Console.ReadLine();
+int num3 = int.Parse(input3);
+
+int[] i = { num1, num2, num3 };
+int maxValue = i.Max();
+
+
+Console.Write($"Максимальное число: {maxValue}");

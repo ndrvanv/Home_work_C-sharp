0996bd82d89d4f7ea0483cc8cbfb8a5637a43247
Andrew <it.andr@outlook.com>
2022-09-17 16:28:42 +0300

    task one

---
+++ task_one/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+string input = Console.ReadLine(); 
+int num1 = int.Parse(input); 
+
+Console.Write("Введите второе число: ");
+string input2 = Console.ReadLine();
+int num2 = int.Parse(input2); 
+
+if (num1 > num2)
+{
+    Console.WriteLine($"max = {num1} min = {num2}");
+} 
+
+else
+{
+    Console.WriteLine($"max = {num2} min = {num1}");
+}

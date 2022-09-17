f582bc9c0ec9fa7994200c18480477534d679aac
Andrew <it.andr@outlook.com>
2022-09-17 22:01:06 +0300

    Thrid task

---
+++ task_three/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+String input = Console.ReadLine();
+int num = int.Parse(input);
+if (num % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+}

dcee22d5310608db66ac62153b7786c3f7089759
Andrew <it.andr@outlook.com>
2022-09-18 14:14:33 +0300

    finish

---
+++ task_four/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string input = Console.ReadLine();
+int num = Math.Abs(int.Parse(input));
+
+int count = 2;
+
+if (num % 2 == 0)
+{
+    while (num >= count)
+{
+    Console.Write($"{count} ");
+    count = count + 2;
+}
+}
+
+else if (num % 2 != 0)
+{
+    num = num - 1;
+    while (num >= count)
+    {
+      Console.Write($"{count} ");
+    count = count + 2;  
+    }
+}

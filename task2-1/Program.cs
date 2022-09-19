e1ee9c488956c5b22dc4f50eaad5ac4a42d52e26
Andrew <it.andr@outlook.com>
2022-09-20 00:28:11 +0300

    Seminar 2 first task

---
+++ task2-1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (100 <= a && a <= 999)
+{
+    Console.WriteLine(a / 10 % 10);
+}
+else
+{
+    Console.WriteLine("Введите трехзначное число!");
+}

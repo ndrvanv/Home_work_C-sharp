1b5b9c73e45c94de238680901b3d8d7dc9752856
Andrew <it.andr@outlook.com>
2022-09-20 19:39:24 +0300

    task 2 2

---
+++ task2-2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string a = Console.ReadLine();
+char[] ch = a.ToCharArray();  
+int length = a.Length;   
+if (ch[0] == '-')
+{
+    Console.Write(ch[3]);
+}
+else if (length < 3)
+{
+    Console.WriteLine("Нет третьей цифры");
+}
+else
+{
+    Console.WriteLine(ch[2]);
+}

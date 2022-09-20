fb77614546af91fb56cb8ea49f60229bd6382534
Andrew <it.andr@outlook.com>
2022-09-20 21:03:16 +0300

    Task 2 part 3

---
+++ task2-3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите число дня недели: ");
+int num = int.Parse(Console.ReadLine());
+
+if (0 < num && num <= 7)
+{
+    if (num <= 5)
+    {
+        Console.WriteLine("Нет");
+    }
+    else
+    {
+        Console.WriteLine("Да");
+    }
+}
+else
+{
+    Console.Write("Введите число от одного до семи!");
+}

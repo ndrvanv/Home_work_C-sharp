2303da51e69cda73e3237a06d6093bd3f1cc0506
Andrew <it.andr@outlook.com>
2022-09-21 23:50:43 +0300

    Additional ver task 2.2

--- task2-2/Program.cs
+++ task2-2/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Clear();
+﻿/*Console.Clear();
 Console.Write("Введите число: ");
 string a = Console.ReadLine();
 char[] ch = a.ToCharArray();  
@@ -14,4 +14,30 @@ else if (length < 3)
 else
 {
     Console.WriteLine(ch[2]);
+}*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int num1 = Math.Abs(num);
+
+if (num1 < 100)
+{
+    Console.WriteLine("Введите трехзначное число!: ");
+}
+else if (num1 >= 100 && num1 <= 999)
+{
+    Console.WriteLine(num1 % 10);
+}
+else if (num1 > 999)
+{
+    while (num1 > 999)
+    {
+        num1 = num1 / 10;
+        if (num1 <= 999 && num1 >= 100)
+        {
+            Console.WriteLine(num1 % 10);
+        }
+        
+    }
 }
